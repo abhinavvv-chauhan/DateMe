8734f10de44093a2c1acfc476635e26a4e2a3617
abhinav <abhinavchauhan593@gmail.com>
2025-06-24 23:00:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./App.css";
+import bear from "./assets/bear.png"
+
+function App() {
+  const [noButtonPosition, setNoButtonPosition] = useState({ left: "50%", top: "70%" });
+  const [showYay, setShowYay] = useState(false);
+
+  const handleYesClick = () => {
+    setShowYay(true);
+  };
+
+  const moveNoButton = (e) => {
+    const button = e.target;
+    
+    const margin = 100;
+    const maxX = window.innerWidth - 600 - margin;
+    const maxY = window.innerHeight - 400 - margin; 
+    
+    const x = Math.max(margin, Math.min(maxX, Math.random() * (maxX - margin) + margin));
+    const y = Math.max(margin, Math.min(maxY, Math.random() * (maxY - margin) + margin));
+    
+    button.style.position = 'absolute';
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+    button.style.zIndex = '20';
+    button.style.transform = 'none';
+  };
+
+  return (
+    <div className="w-full h-screen bg-night relative overflow-hidden flex justify-center items-center">
+      
+      <div className="absolute w-full h-full bg-stars z-0" />
+
+      <div className="relative z-10 backdrop-blur-lg bg-white/10 border border-white/30 shadow-2xl w-[450px] max-w-[90vw] h-[590px] max-h-[90vh] rounded-3xl flex flex-col justify-between p-8 sm:p-9 text-white">
+        <div className="text-center -mt-2 text-3xl sm:text-4xl font-semibold">
+          WOULD YOU LIKE <br /> TO GO ON A DATE <br /> WITH ME ??
+        </div>
+        <img src={bear} className="absolute sm:mt-35 sm:ml-8 mt-30 ml-17 sm:w-60 sm:h-60 md:w-80 md:h-80 w-44 h-44 object-cover z-0" />
+
+
+        <div className="flex justify-center gap-3 sm:gap-6">
+          <button onClick={handleYesClick} className="sm:px-6 sm:py-2 px-4 py-1 cursor-pointer bg-blue-300 hover:bg-green-300 transition rounded-full text-3xl shadow-md text-white font-semibold">
+            Yes
+          </button>
+          <button
+            onClick={moveNoButton}
+            className="sm:px-6 sm:py-2 px-4 py-1 cursor-pointer bg-black hover:bg-red-400 transition rounded-full text-3xl shadow-md text-white font-semibold"
+          >
+            No
+          </button>
+        </div>
+      </div>
+
+      {showYay && (
+        <div className="fixed inset-0 bg-black opacity-85 flex items-center justify-center z-50">
+          <div className="text-white text-9xl font-bold animate-bounce">
+            YAYYYYYY!!!!! 
+          </div>
+        </div>
+      )}
+    </div>
+    
+  );
+}
+
+export default App;
+
